32cb6822be363c7c865cdd231475e60de93413ee
Simon G <simon.gockner@gmail.com>
2024-03-21 10:40:09 +0100

    - add existing tests

---
+++ DatesTest.cs
@@ -0,0 +1,156 @@
+﻿// Author: Simon Gockner
+// Created: 2022-11-11
+// Copyright(c) 2022 SimonG. All Rights Reserved.
+
+using Lib.Tools;
+using NUnit.Framework;
+
+namespace Test.Lib.Tools;
+
+[TestFixture]
+public class DatesTest
+{
+    [Test]
+    public void TestGetDatesOfWeekStartMonday()
+    {
+        DateTime dateTime = new(2022, 11, 7);
+        List<DateTime> datesOfWeek = dateTime.GetDatesOfWeek();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(datesOfWeek[0].Day, Is.EqualTo(7));
+            Assert.That(datesOfWeek[1].Day, Is.EqualTo(8));
+            Assert.That(datesOfWeek[2].Day, Is.EqualTo(9));
+            Assert.That(datesOfWeek[3].Day, Is.EqualTo(10));
+            Assert.That(datesOfWeek[4].Day, Is.EqualTo(11));
+            Assert.That(datesOfWeek[5].Day, Is.EqualTo(12));
+            Assert.That(datesOfWeek[6].Day, Is.EqualTo(13));
+        });
+    }
+    
+    [Test]
+    public void TestGetDatesOfWeekStartTuesday()
+    {
+        DateTime dateTime = new(2022, 11, 8);
+        List<DateTime> datesOfWeek = dateTime.GetDatesOfWeek();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(datesOfWeek[0].Day, Is.EqualTo(7));
+            Assert.That(datesOfWeek[1].Day, Is.EqualTo(8));
+            Assert.That(datesOfWeek[2].Day, Is.EqualTo(9));
+            Assert.That(datesOfWeek[3].Day, Is.EqualTo(10));
+            Assert.That(datesOfWeek[4].Day, Is.EqualTo(11));
+            Assert.That(datesOfWeek[5].Day, Is.EqualTo(12));
+            Assert.That(datesOfWeek[6].Day, Is.EqualTo(13));
+        });
+    }
+    
+    [Test]
+    public void TestGetDatesOfWeekStartWednesday()
+    {
+        DateTime dateTime = new(2022, 11, 9);
+        List<DateTime> datesOfWeek = dateTime.GetDatesOfWeek();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(datesOfWeek[0].Day, Is.EqualTo(7));
+            Assert.That(datesOfWeek[1].Day, Is.EqualTo(8));
+            Assert.That(datesOfWeek[2].Day, Is.EqualTo(9));
+            Assert.That(datesOfWeek[3].Day, Is.EqualTo(10));
+            Assert.That(datesOfWeek[4].Day, Is.EqualTo(11));
+            Assert.That(datesOfWeek[5].Day, Is.EqualTo(12));
+            Assert.That(datesOfWeek[6].Day, Is.EqualTo(13));
+        });
+    }
+    
+    [Test]
+    public void TestGetDatesOfWeekStartThursday()
+    {
+        DateTime dateTime = new(2022, 11, 10);
+        List<DateTime> datesOfWeek = dateTime.GetDatesOfWeek();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(datesOfWeek[0].Day, Is.EqualTo(7));
+            Assert.That(datesOfWeek[1].Day, Is.EqualTo(8));
+            Assert.That(datesOfWeek[2].Day, Is.EqualTo(9));
+            Assert.That(datesOfWeek[3].Day, Is.EqualTo(10));
+            Assert.That(datesOfWeek[4].Day, Is.EqualTo(11));
+            Assert.That(datesOfWeek[5].Day, Is.EqualTo(12));
+            Assert.That(datesOfWeek[6].Day, Is.EqualTo(13));
+        });
+    }
+    
+    [Test]
+    public void TestGetDatesOfWeekStartFriday()
+    {
+        DateTime dateTime = new(2022, 11, 11);
+        List<DateTime> datesOfWeek = dateTime.GetDatesOfWeek();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(datesOfWeek[0].Day, Is.EqualTo(7));
+            Assert.That(datesOfWeek[1].Day, Is.EqualTo(8));
+            Assert.That(datesOfWeek[2].Day, Is.EqualTo(9));
+            Assert.That(datesOfWeek[3].Day, Is.EqualTo(10));
+            Assert.That(datesOfWeek[4].Day, Is.EqualTo(11));
+            Assert.That(datesOfWeek[5].Day, Is.EqualTo(12));
+            Assert.That(datesOfWeek[6].Day, Is.EqualTo(13));
+        });
+    }
+    
+    [Test]
+    public void TestGetDatesOfWeekStartSaturday()
+    {
+        DateTime dateTime = new(2022, 11, 12);
+        List<DateTime> datesOfWeek = dateTime.GetDatesOfWeek();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(datesOfWeek[0].Day, Is.EqualTo(7));
+            Assert.That(datesOfWeek[1].Day, Is.EqualTo(8));
+            Assert.That(datesOfWeek[2].Day, Is.EqualTo(9));
+            Assert.That(datesOfWeek[3].Day, Is.EqualTo(10));
+            Assert.That(datesOfWeek[4].Day, Is.EqualTo(11));
+            Assert.That(datesOfWeek[5].Day, Is.EqualTo(12));
+            Assert.That(datesOfWeek[6].Day, Is.EqualTo(13));
+        });
+    }
+    
+    [Test]
+    public void TestGetDatesOfWeekStartSunday()
+    {
+        DateTime dateTime = new(2022, 11, 13);
+        List<DateTime> datesOfWeek = dateTime.GetDatesOfWeek();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(datesOfWeek[0].Day, Is.EqualTo(7));
+            Assert.That(datesOfWeek[1].Day, Is.EqualTo(8));
+            Assert.That(datesOfWeek[2].Day, Is.EqualTo(9));
+            Assert.That(datesOfWeek[3].Day, Is.EqualTo(10));
+            Assert.That(datesOfWeek[4].Day, Is.EqualTo(11));
+            Assert.That(datesOfWeek[5].Day, Is.EqualTo(12));
+            Assert.That(datesOfWeek[6].Day, Is.EqualTo(13));
+        });
+    }
+    
+    [Test]
+    public void TestGetDatesOfWeekWeekOverlappingMonths()
+    {
+        DateTime dateTime = new(2022, 11, 2);
+        List<DateTime> datesOfWeek = dateTime.GetDatesOfWeek();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(datesOfWeek[0].Day, Is.EqualTo(31));
+            Assert.That(datesOfWeek[1].Day, Is.EqualTo(1));
+            Assert.That(datesOfWeek[2].Day, Is.EqualTo(2));
+            Assert.That(datesOfWeek[3].Day, Is.EqualTo(3));
+            Assert.That(datesOfWeek[4].Day, Is.EqualTo(4));
+            Assert.That(datesOfWeek[5].Day, Is.EqualTo(5));
+            Assert.That(datesOfWeek[6].Day, Is.EqualTo(6));
+        });
+    }
+}
---
+++ EnumerablesTest.cs
@@ -0,0 +1,89 @@
+﻿// Author: Simon.Gockner
+// Created: 2024-03-08
+// Copyright(c) 2024 SimonG. All Rights Reserved.
+
+using Lib.Tools;
+using NUnit.Framework;
+
+namespace Test.Lib.Tools;
+
+[TestFixture]
+public class EnumerablesTest
+{
+    [Test]
+    public void TestMaintainNormalUpdate()
+    {
+        List<string> list = ["1", "2", "3", "4", "5"];
+        List<string> updatedList = ["2", "3", "5", "6"];
+
+        list.Maintain(updatedList);
+        
+        Assert.That(list, Is.EquivalentTo(updatedList));
+    }
+    
+    [Test]
+    public void TestMaintainItemsAdded()
+    {
+        List<string> list = ["1", "2", "3", "4", "5"];
+        List<string> updatedList = ["1", "2", "3", "4", "5", "6", "7"];
+
+        list.Maintain(updatedList);
+        
+        Assert.That(list, Is.EquivalentTo(updatedList));
+    }
+    
+    [Test]
+    public void TestMaintainBeginningRemoved()
+    {
+        List<string> list = ["1", "2", "3", "4", "5"];
+        List<string> updatedList = ["3", "4", "5"];
+
+        list.Maintain(updatedList);
+        
+        Assert.That(list, Is.EquivalentTo(updatedList));
+    }
+    
+    [Test]
+    public void TestMaintainMiddleRemoved()
+    {
+        List<string> list = ["1", "2", "3", "4", "5"];
+        List<string> updatedList = ["1", "3", "5"];
+
+        list.Maintain(updatedList);
+        
+        Assert.That(list, Is.EquivalentTo(updatedList));
+    }
+    
+    [Test]
+    public void TestMaintainRemovedAndReordered()
+    {
+        List<string> list = ["1", "2", "3", "4", "5"];
+        List<string> updatedList = ["5", "1", "3"];
+
+        list.Maintain(updatedList);
+        
+        Assert.That(list, Is.EquivalentTo(updatedList));
+    }
+    
+    [Test]
+    public void TestMaintainRemovedReorderedAdded()
+    {
+        List<string> list = ["1", "2", "3", "4", "5"];
+        List<string> updatedList = ["7", "4", "1", "3", "2", "6", "5"];
+
+        list.Maintain(updatedList);
+        
+        Assert.That(list, Is.EquivalentTo(updatedList));
+    }
+    
+    [Test]
+    public void TestMaintainAddedSameItemFails()
+    {
+        List<string> list = ["1", "2", "3", "4", "5"];
+        List<string> updatedList = ["1", "3", "5", "3"];
+
+        list.Maintain(updatedList);
+        
+        Assert.That(list, Is.EquivalentTo(new List<string> {"1", "3", "5"}));
+    }
+}
